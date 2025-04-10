e684be9f19b743fee8f6d73488faec5662c3fafc
Josiane Steiernagel <josicris3736@gmail.com>
2025-04-09 21:29:45 -0300

    inicio

--- src/main/java/org/scam/Main.java
+++ src/main/java/org/scam/Main.java
@@ -1,7 +1,26 @@
 package org.scam;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Oi");
+
+        Scanner sc = new Scanner(System.in);
+        int continuar = 0;
+
+        do {
+
+            System.out.println("======== SISTEMA DE MENTORIA ========");
+            System.out.println("= [1] - Coordenador                 =");
+            System.out.println("= [2] - Mentor                      =");
+            System.out.println("= [3] - Aluno                       =");
+            System.out.println("=====================================\n");
+            continuar = sc.nextInt();
+
+
+
+        }while (continuar != 4);
+
+
     }
 }
