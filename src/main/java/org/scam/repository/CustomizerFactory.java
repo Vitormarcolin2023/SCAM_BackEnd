f18b64865e1f7c7073b6401a087f84a9aff189f2
unknown <marcolinvh@gmail.com>
2025-04-09 22:04:44 -0300

    Subindo o CustomizerFactory, mas está com erro, ainda falta verificar

--- src/main/java/org/scam/repository/CustomizerFactory.java
+++ src/main/java/org/scam/repository/CustomizerFactory.java
@@ -1,4 +1,30 @@
 package org.scam.repository;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.security.auth.login.Configuration;
+
 public class CustomizerFactory {
+
+    private static final EntityManagerFactory emf;
+
+    static {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+
+        emf = sessionFactory.unwrap(EntityManagerFactory.class);
+    }
+
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+
+    public static void fechar(){
+        emf.close();
+    }
 }
