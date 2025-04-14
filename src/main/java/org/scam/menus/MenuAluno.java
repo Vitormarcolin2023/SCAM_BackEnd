33a667060325fb28029f3c3e2008947807e6f421
Josiane Steiernagel <josicris3736@gmail.com>
2025-04-14 13:06:35 -0300

    voltando o menuAluno

--- src/main/java/org/scam/menus/MenuAluno.java
+++ src/main/java/org/scam/menus/MenuAluno.java
@@ -1,29 +1,38 @@
 package org.scam.menus;
 import org.scam.classes.*;
 import org.scam.cadastros.ProjetoCadastro;
+import org.scam.entities.ProjetoEntity;
+import org.scam.repository.CustomizerFactory;
+import org.scam.repository.ProjetoRepository;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MenuAluno {
 
     private Aluno aluno;
 
+    private final Scanner sc = new Scanner(System.in);
+    EntityManager em = CustomizerFactory.getEntityManager();
+    ProjetoCadastro projetoCadastro = new ProjetoCadastro();
+    Projeto projeto = new Projeto();
+
+
     public MenuAluno(Aluno aluno) {
         this.aluno = aluno;
     }
 
     public void exibirMenu() {
-
-        ProjetoCadastro projetoCadastro = new ProjetoCadastro();
-
-        Scanner sc = new Scanner(System.in);
         int opcao;
 
         do {
-            System.out.println("\n===== MENU DO ALUNO =====");
+            System.out.println("\n=========== MENU DO ALUNO ===========");
             System.out.println("[1] - Cadastrar Projeto");
-            System.out.println("[2] - Listar Mentores");
-            System.out.println("[3] - Voltar");
+            System.out.println("[2] - Gerenciar Projetos");
+            System.out.println("[3] - Listar Mentores");
+            System.out.println("[4] - Voltar\n");
             System.out.print("Selecione uma opção: ");
             opcao = sc.nextInt();
             sc.nextLine();
@@ -31,16 +40,61 @@ public class MenuAluno {
             switch (opcao) {
                 case 1:
                     projetoCadastro.cadastrarProjeto();
+                    System.out.println("Projeto cadastrado com sucesso!");
                     break;
                 case 2:
-                    //listarMentores();
+                    gerenciarProjetos();
                     break;
                 case 3:
-                    System.out.println("Voltando ao menu principal...");
+                    //listarMentores
+                    break;
+                case 4:
+                    System.out.println("Voltando ao menu principal...\n");
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
-        } while (opcao != 3);
+        } while (opcao != 4);
+    }
+
+    public void gerenciarProjetos() {
+
+        int operacao = 0;
+
+        do {
+            System.out.println("\n======== GERENCIAR PROJETOS ========");
+            System.out.println("[1] - Visualizar projetos");
+            System.out.println("[2] - Editar projetos");
+            System.out.println("[3] - Sair");
+            operacao = sc.nextInt();
+
+            switch (operacao) {
+                case 1: {
+                    ProjetoRepository projetoRepository = new ProjetoRepository(em);
+                    List<ProjetoEntity> listaProjetos = projetoRepository.buscarTodos(aluno.getRa());
+
+                    if (!listaProjetos.isEmpty()) {
+                        for (ProjetoEntity projeto : listaProjetos) {
+                            System.out.println(projeto.getNomeDoProjeto());
+                        }
+                    } else {
+                        System.out.println("Aluno sem projetos.\n");
+                    }
+                    break;
+                }
+                case 2: {
+
+                    break;
+                }
+                case 3: {
+                    System.out.println("Retornando à área do aluno...");
+                    break;
+                }
+                default: {
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+                }
+            }
+        } while (operacao != 3);
     }
 }
