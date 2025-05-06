11b8c0584e62bb3c83e8a0697040a255d9deb2cf
unknown <marcolinvh@gmail.com>
2025-05-06 19:40:44 -0300

    Criando o Entity da Area de Atuação

--- src/main/java/org/scam/entities/AreaDeAtuacaoEntity.java
+++ src/main/java/org/scam/entities/AreaDeAtuacaoEntity.java
@@ -1,4 +1,35 @@
 package org.scam.entities;
 
+import javax.persistence.*;
+
+@Entity(name = "tb_area_de_atuacao")
 public class AreaDeAtuacaoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "descricao")
+    private String descricaoAreaDeAtuacao;
+
+    public AreaDeAtuacaoEntity(int id, String descricaoAreaDeAtuacao) {
+        this.id = id;
+        this.descricaoAreaDeAtuacao = descricaoAreaDeAtuacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricaoAreaDeAtuacao() {
+        return descricaoAreaDeAtuacao;
+    }
+
+    public void setDescricaoAreaDeAtuacao(String descricaoAreaDeAtuacao) {
+        this.descricaoAreaDeAtuacao = descricaoAreaDeAtuacao;
+    }
 }
