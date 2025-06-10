21f41751a206d51f1db6f06da96bd506c6fcc4a2
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-06-10 12:53:05 -0300

    adicionando o método de agendamento de reunião - em desenvolvimento

--- src/main/java/org/scam/model/services/ReuniaoService.java
+++ src/main/java/org/scam/model/services/ReuniaoService.java
@@ -1,34 +1,47 @@
 package org.scam.model.services;
 
+import org.scam.controller.classes.Mentor;
+import org.scam.model.entities.AlunoEntity;
 import org.scam.model.entities.MentorEntity;
+import org.scam.model.entities.ProjetoEntity;
 import org.scam.model.entities.ReuniaoEntity;
 import org.scam.model.repository.CustomizerFactory;
+import org.scam.model.repository.ProjetoRepository;
 import org.scam.model.repository.ReuniaoRepository;
+import org.scam.model.repository.StatusReuniao;
 
 import javax.persistence.EntityManager;
 import javax.swing.table.DefaultTableModel;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public class ReuniaoService {
 
-    EntityManager em = CustomizerFactory.getEntityManager();
-    ReuniaoRepository reuniaoRepository = new ReuniaoRepository(em);
+    private static EntityManager em = CustomizerFactory.getEntityManager();
+    private static ReuniaoRepository reuniaoRepository = new ReuniaoRepository(em);
+    private static ProjetoRepository projetoRepository = new ProjetoRepository(em);
 
     public List<ReuniaoEntity> getReunioes(int ra){
         List<ReuniaoEntity> reunioes = reuniaoRepository.buscarReunioes(ra);
         return reunioes;
     }
 
+    public static List<ProjetoEntity> buscarProjetos(int ra){
+        List<ProjetoEntity> projetos = projetoRepository.buscarTodos(ra);
+        return projetos;
+    }
 
-    public DefaultTableModel gerarTableModel(List<ReuniaoEntity> reunioes){
-        String[] colunas = {"ID", "Nome"};
-        DefaultTableModel modelo =new DefaultTableModel(colunas, 0);
+    public void agendarReuniao(String motivo, LocalDate data, LocalTime hora, String local, MentorEntity mentor, ProjetoEntity projeto){
 
-        for (ReuniaoEntity reuniao : reunioes){
-            Object[] linha = {reuniao.getId()};
-            modelo.addRow(linha);
-        }
-        return modelo;
+        ReuniaoEntity novaReuniao = new ReuniaoEntity();
+
+        novaReuniao.setMotivoReuniao(motivo);
+        novaReuniao.setDataReuniao(data);
+        novaReuniao.setHorarioReuniao(hora);
+        novaReuniao.setLocalReuniao(local);
+        novaReuniao.setMentor(mentor);
+        novaReuniao.setProjeto(projeto);
+        novaReuniao.setStatusReuniao(StatusReuniao.AGUARDANDO_CONFIRMACAO);
     }
 }
