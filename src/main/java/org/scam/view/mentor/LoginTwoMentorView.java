16304d5a854302697333ca220e59c6db72f33e84
cecimoura <cecimoura91@gmail.com>
2025-06-14 14:10:29 -0300

    redirecionando o mentor que desativou a conta

--- src/main/java/org/scam/view/mentor/LoginTwoMentorView.java
+++ src/main/java/org/scam/view/mentor/LoginTwoMentorView.java
@@ -148,7 +148,7 @@ public class LoginTwoMentorView {
                             controller.reativarMentor(mentor);
                             Sessao.setMentorLogado(mentor.toMentor());
                             telaLogin.dispose();
-                            TelaInicialMentor.telaMentor();
+                           CadastroMentorPasso1View.exibirTelaCadastroPasso1();
                         } else {
                             telaLogin.dispose();
                             TelaSelecaoUsuarioView.exibirTelaSelecao();
