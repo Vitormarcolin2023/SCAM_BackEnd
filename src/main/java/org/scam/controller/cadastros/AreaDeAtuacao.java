02f0c4cb7db413efd260457d1cf1d34dd02bb3d5
Lethicia <lethiciamsm@gmail.com>
2025-06-14 18:22:08 -0300

    Revert "Começando a arrumar a parte de ENUM"
    This reverts commit 3d959d30e513f66e0bd6a62daf41fbfa00100d68.

--- src/main/java/org/scam/controller/cadastros/AreaDeAtuacao.java
+++ src/main/java/org/scam/controller/cadastros/AreaDeAtuacao.java
@@ -2,21 +2,12 @@ package org.scam.controller.cadastros;
 
 public enum AreaDeAtuacao {
 
-    AGRARIAS_E_BIOLOGIA("Agrárias e Biologia"),
-    EDUCACAO("Educação"),
-    ENGENHARIA_E_ARQUITETURA("Engenharia e Arquitetura"),
-    GESTAO("Gestão"),
-    SAUDE("Saúde"),
-    SOCIAIS("Sociais"),
-    TECNOLOGIA("Tecnologia");
+    AGRÁRIAS_E_BIOLOGIA,
+    EDUCAÇÃO,
+    ENGENHARIA_E_ARQUITETURA,
+    GESTÃO,
+    SAÚDE,
+    SOCIAIS,
+    TECNOLOGIA
 
-    private final String descricao;
-
-    AreaDeAtuacao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
 }
--- src/main/java/org/scam/controller/classes/TipoMentor.java
+++ src/main/java/org/scam/controller/classes/TipoMentor.java
@@ -1,16 +1,7 @@
 package org.scam.controller.classes;
 
 public enum TipoMentor {
-    VOLUNTARIO("Voluntário"),
-    CONTRATADO("Contratado");
-
-    private final String descricao;
-
-    TipoMentor(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
+    Formado,
+    Experiente,
+    Academico
 }
